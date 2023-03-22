e96d70f2602d6fe6c222ea5291c8348c8d71804c
riadghub <119328612+riadghub@users.noreply.github.com>
2023-03-22 10:00:00 +0100

    Changes

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -31,6 +31,7 @@ class RegistrationController extends AbstractController
                     $form->get('plainPassword')->getData()
                 )
             );
+            $user->setCreatedAt(new \DateTimeImmutable);
 
             $entityManager->persist($user);
             $entityManager->flush();
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,25 +4,74 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
+            ->add('username',TextType::class,[
+                'label' => 'Pseudonyme',
+                'attr' => [
+                    'placeholder' => 'Pseudonyme'
+                ]
+            ])
+            ->add('first_name',TextType::class,[
+                'label' => 'Prénom',
+                'attr' => [
+                    'placeholder' => 'Prénom'
+                ]
+            ])
+            ->add('last_name',TextType::class,[
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'Nom'
+                ]
+            ])
+            ->add('address',TextType::class,[
+                'label' => 'Adresse',
+                'attr' => [
+                    'placeholder' => 'Adresse'
+                ]
+            ])
+            ->add('city',TextType::class,[
+                'label' => 'Ville',
+                'attr' => [
+                    'placeholder' => 'Ville'
+                ]
+            ])
+            ->add('zip_code',TextType::class,[
+                'label' => 'Code Postal',
+                'attr' => [
+                    'placeholder' => 'Code Postal'
+                ]
+            ])
+            ->add('country',TextType::class,[
+                'label' => 'Pays',
+                'attr' => [
+                    'placeholder' => 'Pays'
+                ]
+            ])
+            ->add('email',EmailType::class,[
+                'label' => 'Email',
+                'attr' => [
+                    'placeholder' => 'mail@fyz.fr'
+                ]
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Veuillez accepter les conditions.',
                     ]),
                 ],
             ])
@@ -33,11 +82,11 @@ class RegistrationFormType extends AbstractType
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Veuillez entrer un mot de passe.',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Le mot de passe doit être d\'au-moins {{ limit }} caractères.',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
