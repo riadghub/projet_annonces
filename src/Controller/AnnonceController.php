71d2b44b5bbacd6b63160937ade8daf24e05bd28
riadghub <119328612+riadghub@users.noreply.github.com>
2023-03-27 12:09:18 +0200

    Annonce

--- src/Controller/AnnonceController.php
+++ src/Controller/AnnonceController.php
@@ -3,33 +3,65 @@
 namespace App\Controller;
 
 use App\Entity\Annonce;
-use App\Entity\Categorie;
+use App\Entity\Product;
 use App\Form\AnnonceFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class AnnonceController extends AbstractController
 {
     #[Route('/post', name: 'app_annonce')]
     public function index(Request $request, EntityManagerInterface $entityManager): Response
     {   
-        $categorie = new Categorie();
+        $product = new Product();
         $annonce = new Annonce();
         $form = $this->createForm(AnnonceFormType::class, $annonce);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
 
             $user = $this->getUser();
+            $imageFile = $form->get('photo')->getData();
+            if ($imageFile) {
+                // Generate a unique filename
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $newFilename = $originalFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
+
+                // Move the uploaded file to a directory
+                try {
+                    $imageFile->move(
+                        $this->getParameter('product_images_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // Handle the exception
+                }
+
+                // Set the imageFilename property on the Product entity
+                $product->setPhoto($newFilename);
+            }
+            $productName = $form->get('product')->getData();
+            $productDescription = $form->get('product_description')->getData();
+            $productPrice = $form->get('price')->getData();
+            $product->setName($productName);
+            $product->setDescription($productDescription);
+            $product->setPrice($productPrice);
+            $product->setCreatedAt(new \DateTimeImmutable);
+            
+            $entityManager->persist($product);
+
             $annonce->setCreatedAt(new \DateTimeImmutable);
             $annonce->setAuthor($user);
-            $annonce->setCategorie($categorie);
+            $annonce->setUser($user);
+            $annonce->setProduct($product);
+            $annonce->setCategorie($form->get('categorie')->getData());
 
+            $product->setAnnonce($annonce);
             $entityManager->persist($annonce);
             $entityManager->flush();
-            // $this->addFlash('success', 'Annonce crée avec succès.');
         }
         return $this->render('annonce/annonce.html.twig', [
             'annonceForm' => $form->createView(),
--- src/Form/AnnonceFormType.php
+++ src/Form/AnnonceFormType.php
@@ -3,11 +3,54 @@
 namespace App\Form;
 
 use App\Entity\Annonce;
+use App\Entity\Product;
+use App\Entity\Categorie;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\DataTransformerInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\Form\Exception\TransformationFailedException;
 
+class FileToPhotoTransformer implements DataTransformerInterface
+{
+    private $uploadsDirectory;
+
+    public function __construct($uploadsDirectory)
+    {
+        $this->uploadsDirectory = $uploadsDirectory;
+    }
+
+    public function transform($value)
+    {
+        return null;
+    }
+
+    public function reverseTransform($value)
+    {
+        if (!$value instanceof UploadedFile) {
+            return;
+        }
+
+        $fileName = uniqid() . '.' . $value->guessExtension();
+
+        try {
+            $value->move($this->uploadsDirectory, $fileName);
+        } catch (FileException $e) {
+            throw new TransformationFailedException(sprintf(
+                'An error occurred while uploading the file: %s',
+                $e->getMessage()
+            ));
+        }
+
+        return $fileName;
+    }
+}
 class AnnonceFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
@@ -19,9 +62,32 @@ class AnnonceFormType extends AbstractType
                     'placeholder' => 'Titre de l\'annonce...'
                 ]
             ])
-            ->add('categorie')
-            ->add('product')
-        ;
+            ->add('categorie', EntityType::class, [
+                'class' => Categorie::class,
+                'choice_label' => 'name',
+            ])
+            ->add('product', TextType::class, [
+                'mapped' => false,
+                'label' => 'Produit',
+            ])
+            ->add('product_description', TextType::class, [
+                'mapped' => false,
+                'label' => 'Description',
+            ])
+            ->add('price', TextType::class, [
+                'mapped' => false,
+                'label' => 'Prix',
+            ])
+            ->add('photo', FileType::class, [
+                'label' => 'Image',
+                'required' => false,
+                'mapped' => false,
+                'data_class' => null,
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Poster'
+            ])
+            ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
