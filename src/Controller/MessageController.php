7bb2b04db3394e8acc51990999a67bb417710bd5
riadghub <119328612+riadghub@users.noreply.github.com>
2023-03-28 14:46:34 +0200

    messagerie

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Message;
+use App\Form\MessageFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MessageController extends AbstractController
 {
@@ -13,6 +17,45 @@ class MessageController extends AbstractController
     {
         return $this->render('message/index.html.twig', [
             'controller_name' => 'MessageController',
+        ]);
+    }
+    #[Route('/send', name: 'app_send')]
+    public function send(Request $request, EntityManagerInterface $entityManager): Response{
+        $message = new Message;
+        $form = $this->createForm(MessageFormType::class, $message);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $message->setSender($this->getUser());
+            $message->setCreatedAt(new \DateTimeImmutable());
+            $message->setIsRead(false);
+            $entityManager->persist($message);
+            $entityManager->flush();
+
+            $this->addFlash('message','Message envoyé avec succès.');
+            return $this->redirectToRoute('app_message');
+        }
+        return $this->render('message/send.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+    #[Route('/received', name: 'app_received')]
+    public function received(): Response
+    {
+        return $this->render('message/received.html.twig', [
+            'controller_name' => 'MessageController',
+        ]);
+    }
+    #[Route('/read', name: 'app_read')]
+    public function read(Message $message, EntityManagerInterface $entityManager): Response
+    {
+        $message->setIsRead(true);
+
+        $entityManager->persist($message);
+        $entityManager->flush();
+
+        return $this->render('message/read.html.twig', [
+            'controller_name' => 'MessageController',
+            compact('message'),
         ]);
     }
 }
---
+++ src/Form/MessageFormType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use App\Entity\Message;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class MessageFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title',TextType::class,[
+
+            ])
+            ->add('message',TextareaType::class)
+            ->add('recipient',EntityType::class,[
+                "class" => User::class,
+                "choice_label" => 'email'
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Envoyer'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Message::class,
+        ]);
+    }
+}
